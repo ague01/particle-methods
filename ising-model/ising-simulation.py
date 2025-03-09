fix logging and energy computation

ising-model/ising-simulation.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 seed = 65647437836358831880808032086803839626
 rng = np.random.default_rng(MT19937(seed))
 
+# Avoid warning when dividing by zero (T=0)
+np.seterr(divide='ignore')
+
 
 def get_neighbours(a, b, size):
     return [[(a + 1) % size[0], b],
@@ -20,12 +23,13 @@ def random_initial_state(size):
 
 
 def compute_energy(state, coupling):
-    energy = 0
-    for a in range(state.shape[0]):
-        for b in range(state.shape[1]):
-            h = sum(state[x, y] for x, y in get_neighbours(a, b, state.shape))
-            energy += -coupling * state[a, b] * h
+    energy = - coupling
+    energy *= sum(state[a, b] * state[x, y]
+                  for a in range(state.shape[0])
+                  for b in range(state.shape[1])
+                  for x, y in get_neighbours(a, b, state.shape))
     return energy
+
 
 def compute_energy_diff(state, a, b, coupling):
     h = sum(state[x, y] for x, y in get_neighbours(a, b, state.shape))
@@ -53,16 +57,6 @@ def metropolis(state, temperature, n_steps, coupling, kB):
 
 
 def simulate(size, coupling, kB, temperature, n_therm, n_samples, n_subsweeps):
-    print("Ising Model Simulation")
-    print("-"*30)
-    print(f"Size: {size}")
-    print(f"Coupling: {coupling}")
-    print(f"Temperature: {temperature}")
-    print(f"Thermalization Steps: {n_therm}")
-    print(f"Number of Samples: {n_samples}")
-    print(f"Subsweeps per Sample: {n_subsweeps}")
-    print("-"*30)
-
     # Initialize the lattice with random spins
     state = random_initial_state(size)
     # Run the metropolis algorithm for n_them steps
@@ -75,29 +69,47 @@ def simulate(size, coupling, kB, temperature, n_therm, n_samples, n_subsweeps):
         # Run the metropolis algorithm for n_subsweeps steps
         state = metropolis(state, temperature, n_steps=n_subsweeps, coupling=coupling, kB=kB)
         # Measure the magnetization
-        magnetizations.append(average_magnetization(state))
+        magnetizations.append(np.abs(average_magnetization(state)))
         # Measure the energy
+        energy.append(compute_energy(state, coupling))
     # Compute the time average of the magnetization
     magnetization = time_average(magnetizations)
-    # Plot the magnetization
-    plt.plot(x=range(magnetization), y=magnetizations)
-    plt.xlabel("Steps")
-    plt.ylabel("Magnetization")
-    plt.show()
+    energy = time_average(energy)
 
-    return 0
+    return magnetization, energy
 
 
 def main():
+    out_path = './out/'
+
     size = (10, 10)
-    temperature = 1.0
+    temperatures = [0.0, 1.0, 1.5, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 3.0]
     n_therm = 100_000
     n_samples = 5_000
     n_subsweeps = size[0] * size[1]
     coupling = 1.0
     kB = 1.0
-    simulate(size, coupling, kB, temperature, n_therm, n_samples, n_subsweeps)
+
+    print('Marcov Chain Ising Model Simulation')
+    print('-'*30)
+    print(f'Size: {size}')
+    print(f'Coupling: {coupling}')
+    print(f'Temperatures: {temperatures}')
+    print(f'Thermalization Steps: {n_therm}')
+    print(f'Number of Samples: {n_samples}')
+    print(f'Subsweeps per Sample: {n_subsweeps}')
+    print('-'*30)
+
+    magnetizations = []
+    energies = []
+    for temperature in temperatures:
+        print(f'Temperature: {temperature}')
+        m, e = simulate(size, coupling, kB, temperature, n_therm, n_samples, n_subsweeps)
+        magnetizations.append(m)
+        energies.append(e)
+        print(f'\tMagnetization: {m}')
+        print(f'\tEnergy: {e}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()