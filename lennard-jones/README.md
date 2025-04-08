58365aa84c3e22c9c3da1ee3dd4dbf8dced9c3ee
ague01 <aguerrini01@gmail.com>
2025-04-08 21:56:23 +0200

    add report and plots

---
+++ lennard-jones/README.md
@@ -0,0 +1,69 @@
+# Molecular Dynamics based on Lennar-Jones potential
+
+ This project implements a molecular dynamics simulation using the Lennard-Jones potential to model interactions between particles. The goal is to study the behavior of particle systems under various conditions.
+
+
+## Implementation details
+
+The relevant implementation choices are listed below:
+
+- The data structure for the particles is implemented as a list of cells (i.e. list of `Particle` objects), each containing pointers to `Particle` objects. It should have performance comparable to the usual implementation where cell-list contains the index of a given particle, and particles are stored in another array as pointers are handled in a similar manner to integers. For better performances other programming languages with explicit memory management should be preferred.
+- The particles' initial velocity is drawn from a normal distribution and then normalized to get zero total momentum in each dimension setting the kinetic energy associated to the desired temperature.
+
+## Results
+
+### a) Simulations without termostat
+
+Total momentum is conserved only for limited number of particles in the environment (i.e. $N\le30$), with negligible dependence on the environment size $L$.
+
+These simulations have been run with $T=3$, $N=400$, and $dt\in\{ 0.01, 0.005, 0.0025\}$. Other parameters are set as in the project requirements.
+
+The size of energy fluctuations dependence on the used time step $dt$ is represented in Figure 1.
+
+<figure>
+ <center>
+ <img src="./out/energies_dt2.png"
+   alt="Energy as function of time"
+   width=98%>
+ <figcaption><em> Figure 1: Energy fluctuations for different time steps dt. </em></figcaption>
+ </center>
+</figure>
+
+The fluctuations in the total energy have a irregular behavior that empirically from Figure 1 seems not to be correlated to the step size.
+
+To identify the time steps necessary to reach the equilibrium, Figure 2 shows the evolution in time of the total energy for different $dt$ and the evolution of kinetic and potential energy for $dt=0.0025$.
+
+<figure>
+ <img src="./out/energies_dt.png"
+   alt="Energy as function of time "
+   width=49%>
+ <img src="./out/kinetic_potential_energy.png"
+   alt="Energy as function of time "
+   width=49%>
+ <figcaption><em> Figure 2: (Left) Energy evolution as function of time for different time steps dt. (Right) Kinetic and potential energy for dt=0.0025 as function of time. </em></figcaption>
+</figure>
+
+We observe that from $t\ge3$ time units the simulation reaches an equilibrium for $dt=0.05$ while from $t\ge1.5$ for for $dt=0.0025$. The evolution of kinetic and potential energy for this last $dt$ value, however shows that kinetic energy stabilizes around $t\ge4$. From the left plot we can see that after $k=\frac{1.5}{0.0025}=\frac{3}{0.005}=600$ iterations, the system reaches a stable state.
+
+### b) Simulations with thermostat
+
+The implementation with the Berendsen thermostat is tested with the provided parameters. The results are shown in Figure 3 for the timesteps to reach equilibrium and in Figure 4 for the radial distribution function.
+
+<figure>
+ <img src="./out/kinetic_potential_energy_term.png"
+   alt="Energy as function of time "
+   width=98%>
+
+ <figcaption><em> Figure 3: Energy evolution as function of time. </em></figcaption>
+</figure>
+
+From Figure 3 we can see that the system reaches equilibrium almost instantaneously after initialization for what concerns the kinetic energy, while the potential energy stabilizes around $t\ge1.8$. The Berendsen thermostat seems to work well in maintaining the temperature of the system.
+
+<figure>
+ <img src="./out/radial_distribution_function.png"
+ alt="Energy as function of time "
+ width=98%>
+ <figcaption><em> Figure 4: Radial distribution function of particles within neighbouring cells. </em></figcaption>
+</figure>
+
+The radial distribution function is computed for the system with different parameters and only within the neighbour cells. The plot shows the expected behavior of the radial distribution function, with peaks corresponding to the positions of the particles in the system in an organized structure. However, noise is present in the plot as the system is not perfectly ordered and the initialization is random and not in a lattice structure.
--- lennard-jones/lj_simulation.py
+++ lennard-jones/lj_simulation.py
@@ -1,6 +1,4 @@
-from math import e
-import os
-from pprint import pprint
+from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -131,10 +129,6 @@ class Environment:
         t_kinetic_energy = 0.
         t_potential_energy = 0.
 
-        for cell in self.cells:
-            for particle in cell:
-                assert np.all(particle.force == 0), "Particle force should be initialized to zero."
-
         def func_pot(r6): return 4. / r6 * (1./r6 - 1.)
         ecut = func_pot(Particle._sq_r_cutoff ** 3)
 
@@ -149,7 +143,7 @@ class Environment:
                 # Compute same cell interactions
                 for particle2 in cell1[i_particle+1:]:
                     r2 = self.periodic_squared_distance(particle1.position, particle2.position)
-                    if r2 <= Particle._sq_r_cutoff and r2 > 1e-16:
+                    if r2 <= Particle._sq_r_cutoff and r2 > 1e-4:
                         # b) Potential energy
                         t_potential_energy += (func_pot(r2 ** 3) - ecut)
                         # c) Force
@@ -158,10 +152,9 @@ class Environment:
                         particle1.force += ff
                         particle2.force -= ff
 
-                #Compute neighbouring cell interactions
+                # Compute neighbouring cell interactions
                 for idx_cell2 in self.get_neighbour_cells(i_cell):
                     for particle2 in self.cells[idx_cell2]:
-                        assert particle2 is not particle1
                         r2 = self.periodic_squared_distance(particle1.position, particle2.position)
                         if r2 <= Particle._sq_r_cutoff and r2 > 1e-16:
                             # b) Potential energy
@@ -174,6 +167,33 @@ class Environment:
                             particle2.force -= ff
 
         return t_kinetic_energy, t_potential_energy
+
+    def compute_radial_distribution(self, bin_size: float) -> np.ndarray:
+        """Compute the radial distribution function of the particles."""
+        # Initialize the histogram
+        histogram = defaultdict(int)
+
+        # Compute the radial distribution function
+        for i_cell, cell1 in enumerate(self.cells):
+            # Compute same particle quantities
+            for i_particle, particle1 in enumerate(cell1):
+                # Compute same cell interactions
+                for particle2 in cell1[i_particle+1:]:
+                    r = np.sqrt(self.periodic_squared_distance(
+                        particle1.position, particle2.position))
+                    histogram[int(r // bin_size)] += 1
+
+                # Compute neighbouring cell interactions
+                for idx_cell2 in self.get_neighbour_cells(i_cell):
+                    for particle2 in self.cells[idx_cell2]:
+                        r = np.sqrt(self.periodic_squared_distance(
+                            particle1.position, particle2.position))
+                        histogram[int(r // bin_size)] += 1
+
+        # Convert the histogram to a numpy array
+        max_bin = max(histogram.keys())
+        histogram = np.array([histogram.get(i, 0) for i in range(max_bin + 1)])
+        return histogram
 
     def plot_particles(self, file_id: str = ''):
         """Plot the particles in the environment."""
@@ -246,7 +266,7 @@ def initialize(
     # Randomly initialize velocities according to the Maxwell-Boltzmann distribution at a given T
     velocities = rng.random(size=(n_particles, n_dims)) - 0.5
     # Compute the rescaling factor based on kinetic energy
-    scale_f = np.sqrt(n_dims * T / np.mean(np.square(velocities), axis=0))
+    scale_f = np.sqrt(2 * T / np.mean(np.square(velocities), axis=0))
     # Remove center-of-mass velocity to ensure zero total momentum and correct scaling
     velocities = (velocities - np.mean(velocities, axis=0)) * scale_f
 
@@ -263,7 +283,8 @@ def initialize(
     return env
 
 
-def simulate(env: Environment, dt: float, max_time: float) -> tuple[list, list]:
+def simulate(env: Environment, dt: float, max_time: float, termostat_parameter: float | None = None,
+             T: float | None = None, N: int | None = None) -> tuple[list, list]:
     start_time = time.time()  # Start timing the simulation
     t = 0
     total_kinetic_energy = []
@@ -272,9 +293,9 @@ def simulate(env: Environment, dt: float, max_time: float) -> tuple[list, list]:
     while t < max_time:
         if t % 0.1 < dt:
             print(f'Starting simulation step n={t/dt:.0f} at time t={t:.2f}')
-        total_momentum = env.compute_total_momentum()
-        print(
-            f"Time {t:.2f}: Total Momentum = {total_momentum}, Magnitude = {np.linalg.norm(total_momentum)}")
+            total_momentum = env.compute_total_momentum()
+            print(f"Time {t:.2f} (step {t/dt:.0f}): Total Momentum = {total_momentum},"
+                  f"Magnitude = {np.linalg.norm(total_momentum)}")
 
         # Update particle positions and partially velocities
         for cell in env.cells:
@@ -300,70 +321,191 @@ def simulate(env: Environment, dt: float, max_time: float) -> tuple[list, list]:
             for particle in cell:
                 particle.update_velocity(dt)
 
+        # Apply thermostat if specified
+        if termostat_parameter is not None and T is not None and N is not None:
+            gamma = np.sqrt(1 + termostat_parameter * ( T / (t_kin / N * 2./3.) - 1))
+            for cell in env.cells:
+                for particle in cell:
+                    particle.velocity *= gamma
         # Increment time
         t += dt
 
     # Print simulation duration
-    print(f"Simulation completed in {time.time() - start_time:.2f} seconds.")
+    print(
+        f"Simulation of {max_time/dt} time steps completed in {time.time() - start_time:.2f} seconds.")
 
     return total_kinetic_energy, total_potential_energy
 
 
-def main():
-    n_dims = 2
-    domain_size = 2.5
+def main_task_preliminary():
 
-    n_particles = 4
+    n_dims = 2
+    domain_size = 30.
+
+    n_particles = [100, 300, 600, 900]
     mass = 1.
-    T = 0.8
+    T = [0.1, 0.5, 1.0]
     r_cutoff = 2.5
 
-    max_time = 0.1
+    max_time = 10.0
     dt = 0.01
 
-    # Initialize the environment and particles
-    env = initialize(n_particles, domain_size, n_dims, T, mass, r_cutoff, dt)
-    env.plot_particles(file_id='initial')
-    env.plot_forces(file_id='initial')
-    # Store the total momentum and energies
-    initial_momentum = env.compute_total_momentum()
+    # Simulate the environment for different values of T and n_particles
+    # Check for momentum conservation
+    for n in n_particles:
+        for t in T:
+            print(f"Simulating with n_particles={n}, T={t}")
+            env = initialize(n, domain_size, n_dims, t, mass, r_cutoff, dt)
+            initial_momentum = env.compute_total_momentum()
+            t_kin, t_pot = simulate(env, dt, max_time)
+            final_momentum = env.compute_total_momentum()
 
-    min_distance = np.inf
-    for cell in env.cells:
-        for p1 in cell:
-            for p2 in cell:
-                if p1 is not p2:
-                    r2 = env.periodic_squared_distance(p1.position, p2.position)
-                    min_distance = min(min_distance, np.sqrt(r2))
-    print(f"Minimum distance between particles: {min_distance:.2f}")
+            # Print the difference in total momentum and energies
+            print("\tInitial Total Momentum:", initial_momentum)
+            print("\tFinal Total Momentum:", final_momentum)
+            print(f"\tInitial Total Energy: {t_kin[0] + t_pot[0]:e}")
+            print(f"\tFinal Total Energy: {t_kin[-1] + t_pot[-1]:e}")
 
-    # Simulate the environment
-    t_kin, t_pot = simulate(env, dt, max_time)
 
-    # Plot the energies as function of time
+def main_task_a():
+    n_dims = 2
+    domain_size = 30.
+
+    n_particles = 400
+    mass = 1.
+    T = 0.3
+    r_cutoff = 2.5
+
+    max_time = 10.0
+    dt = 0.01
+
+    # Simulate the environment for different values of dt
+    dt_values = [0.01, 0.005, 0.0025]
+
+    # Prepare the plot
     fig, ax = plt.subplots()
-    ax.plot(np.arange(len(t_kin)) * dt, t_kin, label='Kinetic Energy')
-    ax.plot(np.arange(len(t_pot)) * dt, t_pot, label='Potential Energy')
-    ax.plot(np.arange(len(t_kin)) * dt, np.array(t_kin) + np.array(t_pot), label='Total Energy')
-    ax.set_title('Kinetic and Potential Energy over Time')
+    ax.set_title('Total Energy over Time for Different dt Values')
     ax.set_xlabel('Time')
     ax.set_ylabel('Energy')
-    ax.legend()
     ax.set_yscale('log')
     ax.grid(True, which='both')
-    plt.savefig('./out/energies.png')
+
+    fig2, ax2 = plt.subplots(1, len(dt_values), sharex=True, figsize=(15, 4))
+
+    t_kin = []
+    t_pot = []
+
+    for i, dt in enumerate(dt_values):
+        print(f"Simulating with dt={dt}")
+        env = initialize(n_particles, domain_size, n_dims, T, mass, r_cutoff, dt)
+        initial_momentum = env.compute_total_momentum()
+        t_kin, t_pot = simulate(env, dt, max_time)
+        final_momentum = env.compute_total_momentum()
+        total_energy = np.array(t_kin) + np.array(t_pot)
+        ax.plot(np.arange(len(total_energy)) * dt, total_energy, label=f'Total Energy (dt={dt})')
+
+        # Plot the total energy from t=8
+        total_energy = total_energy[int(9.0/dt):int(9.5/dt)]
+        total_energy -= np.mean(total_energy)  # Normalize to zero
+        ax2[i].plot(np.arange(len(total_energy)) * dt + 8, total_energy)
+        ax2[i].set_title(f'Energy Fluctuations (dt={dt})')
+        ax2[i].set_xlabel('Time')
+        ax2[i].set_ylabel('Total Energy - avg. Total Energy')
+        ax2[i].grid(True, which='both')
+
+        # Print the difference in total momentum and energies
+        print("Initial Total Momentum:", initial_momentum)
+        print("Final Total Momentum:", final_momentum)
+        print(f"Initial Total Energy: {t_kin[0] + t_pot[0]:e}")
+        print(f"Final Total Energy: {t_kin[-1] + t_pot[-1]:e}")
+
+    # Finalize the plot
+    ax.legend()
+    fig.savefig('./out/energies_dt.png')
     plt.close(fig)
 
-    env.plot_particles(file_id='final')
-    final_momentum = env.compute_total_momentum()
+    fig2.savefig('./out/energies_dt2.png')
+    plt.close(fig2)
 
-    # Print the difference in total momentum and energies
-    print("Initial Total Momentum:", initial_momentum)
-    print("Final Total Momentum:", final_momentum)
-    print(f"Initial Total Energy: {t_kin[0] + t_pot[0]:e}")
-    print(f"Final Total Energy: {t_kin[-1] + t_pot[-1]:e}")
+    # Plot the kinetic and potential energies as function of time
+    fig, ax1 = plt.subplots()
+
+    ax1.set_title('Kinetic and Potential Energy over Time')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Kinetic Energy', color='tab:blue')
+    ax1.set_yscale('log')
+    ax1.plot(np.arange(len(t_kin)) * dt, t_kin, label='Kinetic Energy', color='tab:blue')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+    ax1.grid(True, which='both')
+
+    ax2 = ax1.twinx()  # Create a second y-axis sharing the same x-axis
+    ax2.set_ylabel('Potential Energy', color='tab:orange')
+    ax2.set_yscale('log')
+    ax2.plot(np.arange(len(t_pot)) * dt, t_pot, label='Potential Energy', color='tab:orange')
+    ax2.tick_params(axis='y', labelcolor='tab:orange')
+
+    fig.tight_layout()  # Adjust layout to prevent overlap
+
+    plt.savefig('./out/kinetic_potential_energy.png')
+    plt.close(fig)
+
+
+def main_task_b():
+    n_dims = 2
+    domain_size = 30.
+    mass = 1.
+    r_cutoff = 2.5
+    max_time = 5.0
+    dt = 0.01
+
+    parameters = [(100, 0.1), (100, 1.0), (625, 1.0), (900, 1.0)]
+
+    # Prepare the plot
+    fig, ax = plt.subplots()
+    ax.set_title('Radial Distribution Function')
+    ax.set_xlabel('Distance')
+    ax.set_ylabel('g(r)')
+    ax.grid(True, which='both')
+    bin_size = 0.1
+
+    # For each parameter set, run the simulation and plot the radial distribution function
+    for n_particles, T in parameters:
+        print(f"Simulating with n_particles={n_particles}, T={T}")
+        env = initialize(n_particles, domain_size, n_dims, T, mass, r_cutoff, dt)
+        t_kin, t_pot = simulate(env, dt, max_time, termostat_parameter=0.0025, T=T, N=n_particles)
+        # Compute and plot the radial distribution function
+        rdf = env.compute_radial_distribution(bin_size)
+        distances = np.arange(len(rdf)) * bin_size
+        avg_rdf = np.mean(rdf)
+        ax.plot(distances, rdf / avg_rdf, label=f'n={n_particles}, T={T}')
+
+    # Finalize the plot
+    ax.legend()
+    plt.savefig('./out/radial_distribution_function.png')
+    plt.close(fig)
+
+    # Plot the kinetic and potential energies as function of time
+    fig, ax1 = plt.subplots()
+    ax1.set_title('Energies over Time with Thermostat')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Kinetic Energy', color='tab:blue')
+    ax1.set_yscale('log')
+    ax1.plot(np.arange(len(t_kin)) * dt, t_kin, label='Kinetic Energy', color='tab:blue')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+    ax1.grid(True, which='both')
+    ax2 = ax1.twinx()  # Create a second y-axis sharing the same x-axis
+    ax2.set_ylabel('Potential Energy', color='tab:orange')
+    ax2.set_yscale('log')
+    ax2.plot(np.arange(len(t_pot)) * dt, t_pot, label='Potential Energy', color='tab:orange')
+    ax2.tick_params(axis='y', labelcolor='tab:orange')
+    fig.tight_layout()  # Adjust layout to prevent overlap
+    plt.savefig('./out/kinetic_potential_energy_term.png')
+    plt.close(fig)
+
+    return
 
 
 if __name__ == '__main__':
-    # Run the main simulation
-    main()
+    # main_task_preliminary()
+    main_task_a()
+    main_task_b()
